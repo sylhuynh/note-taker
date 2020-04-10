2c1c5daa6b4d0291251d7a367754c397e8295b2f
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-09 21:06:26 -0700

    able to read db.json file

--- server.js
+++ server.js
@@ -11,11 +11,46 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let notes = [];
 
 //Routes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
+
+
+app.get("/api/notes", (req, res) => {
+    const filePath = path.join(__dirname, "/db/db.json");
+
+    fs.readFile(filePath, "utf8", (err,data)=>{
+        if (err) throw err;
+
+        const parsedData = JSON.parse(data);
+        res.json(parsedData);
+    });
+  });
+
+// app.post("/api/notes", (req, res) => {
+//     const newNote = req.body;
+//     console.log(newNote);
+    
+//     notes.push(newNote);
+    
+//     fs.writeFile("/db/db.json", JSON.stringify(notes), function(err){
+//         if (err) throw err;
+    
+//     });
+
+//     res.json(newNote);
+//   });
+
+
+
+
+
+
+
+
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
